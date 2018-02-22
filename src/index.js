45e8398246b0dd5db338f0d0bff82096cce2179c
Siarhei <solimr111@mail.ru>
2018-02-22 19:44:25 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+	let answer= 0, index = 5;
+
+	while(number > (5^index)){
+		answer += parseInt(number/index);
+		index *= 5;
+	}
+	return answer;
 }
